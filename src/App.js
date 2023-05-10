9be15d9edba030504e5ef982bd19dcd83a868522
juliana <julianapereira.dev@gmail.com>
2023-05-09 23:46:57 -0300

    Removi código desnecessário no index + acrescentei Array de palavras de forma aleatória

--- src/App.js
+++ src/App.js
@@ -4,8 +4,16 @@ import palavras from './palavras';
 
 
 function App() {
+
+  palavras.sort(comparador);
+
+  function comparador() {
+    return Math.random() - 0.5;
+  }
+
   return (
     <div className='App'>
+      {console.log('mostrando palavras dentro do App:', palavras)}
       <Jogo />
       <div className='keyboard-center'>
         <Letras />
