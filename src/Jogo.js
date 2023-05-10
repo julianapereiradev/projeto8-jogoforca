1f4c393488dcd9d50ab88cb675c2160c8fab6849
juliana <julianapereira.dev@gmail.com>
2023-05-10 00:46:48 -0300

    Implementei disabled no HTML do teclado

--- src/Jogo.js
+++ src/Jogo.js
@@ -1,16 +1,21 @@
 import forca0 from './assets/forca0.png'
 
 export default function Jogo() {
+
+function chooseWord() {
+    alert('Cliquei')
+}
+
     return (
         <div className="board-container">
             <div className="board-left">
-                <img src={forca0} />
+                <img src={forca0} alt="forca0" />
             </div>
             <div className='board-right'>
-                <button className='button-sort-word'>
+                <button className='button-sort-word' onClick={chooseWord}>
                     Escolher Palavra
                 </button>
-                <div className='letter-container'>
+                <div className='letter-container display-none'>
                     <p className='letter-underline'></p>
                     <p className='letter-underline'></p>
                     <p className='letter-underline'></p>
--- src/Letras.js
+++ src/Letras.js
@@ -1,11 +1,14 @@
-export default function Letras() {
+export default function Letras(props) {
 
     const alfabeto = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
 
     return (
         <div className="keyboard-container">
             {alfabeto.map((letra) =>
-                <button className="button-key">
+                <button
+                disabled={true}
+                className="button-key"
+                >
                     <p>{letra.toUpperCase()}</p>
                 </button>
             )}
